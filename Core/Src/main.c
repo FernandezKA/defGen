3324b11d373d5dcdd43da57f525468dd693b0a26
Fernandez K.A <fernandes.kir@yandex.ru>
2021-04-17 18:06:19 +0300

    Edit frequency of lhe led_1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -297,7 +297,7 @@ static void MX_TIM2_Init(void)
   /* USER CODE END TIM2_Init 1 */
   TIM_InitStruct.Prescaler = 7199;
   TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
-  TIM_InitStruct.Autoreload = 3332;
+  TIM_InitStruct.Autoreload = 1666;
   TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
   LL_TIM_Init(TIM2, &TIM_InitStruct);
   LL_TIM_EnableARRPreload(TIM2);
